4e98118d561f86f3c0a2574edf2498efd025d2cf
amy <amykim4229@gmail.com>
2022-10-13 12:56:08 +0900

    #6 init [DEV] 스프링부트 프로젝트 초기화
    스프링부트 프로젝트 초기화
    
    추가한 개발 디펜던시
       롬복, spring-web, actuator, devtools
    gitignore작성

---
+++ src/main/java/com/amy/springboard/AmySpringBoardApplication.java
@@ -0,0 +1,13 @@
+package com.amy.springboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmySpringBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmySpringBoardApplication.class, args);
+    }
+
+}
