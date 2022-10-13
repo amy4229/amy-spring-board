4e48a0b27e13b8b15c6c621e88a5a582c4b698f1
amy <amykim4229@gmail.com>
2022-10-13 14:47:14 +0900

    #8 UPD 도메인 클래스 주석추가
    도메인 설명을 위한 주석 추가

--- src/main/java/com/amy/springboard/domain/Comment.java
+++ src/main/java/com/amy/springboard/domain/Comment.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class Comment {
     private Long id;
-    private Post post;
-    private String content;
+    private Post post;  // 게시글
+    private String content; //본문
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt;    //최초등록일자
+    private String createdBy;   // 최초등록자
+    private LocalDateTime modifiedAt; // 최종수정일자
+    private String modifiedBy;  //최종 수정자
 }
--- src/main/java/com/amy/springboard/domain/Post.java
+++ src/main/java/com/amy/springboard/domain/Post.java
@@ -4,12 +4,12 @@ import java.time.LocalDateTime;
 
 public class Post {
     private Long id;
-    private String title;
-    private String content;
-    private String tag;
+    private String title;   // 제목
+    private String content; // 본문
+    private String tag;     //해시태그
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    private LocalDateTime createdAt;    //최초등록일자
+    private String createdBy;   // 최초등록자
+    private LocalDateTime modifiedAt; // 최종수정일자
+    private String modifiedBy;  //최종 수정자
 }
