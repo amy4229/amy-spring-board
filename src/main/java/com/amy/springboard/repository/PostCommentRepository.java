4340c8bd5ac8dc5faa6cbd7fbf484ac95c6d02ff
amy <amykim4229@gmail.com>
2022-10-26 11:49:08 +0900

    #12 게시글, 댓글에 검색기능 추가
    Querydsl을 활용 
    게시글:  제목, 본문, 최초작성자, 최초작성일자, 태그의 검색기능추가
    댓글 : 본문, 최초작성일자, 최초작성자
    
    최초작성일자: 완전일치
    최초작성일자 제외한 항목 : 부분일치, 대소문자구분없이 검색가능

--- src/main/java/com/amy/springboard/repository/PostCommentRepository.java
+++ src/main/java/com/amy/springboard/repository/PostCommentRepository.java
@@ -1,9 +1,29 @@
 package com.amy.springboard.repository;
 
 import com.amy.springboard.domain.PostComment;
+import com.amy.springboard.domain.QPostComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PostCommentRepository extends JpaRepository<PostComment, Long> {
+public interface PostCommentRepository extends
+        JpaRepository<PostComment, Long>,
+        QuerydslPredicateExecutor<PostComment>,
+        QuerydslBinderCustomizer<QPostComment> {
+
+    @Override
+    default void customize(QuerydslBindings bindings, QPostComment root){
+        // 사용자가 정의한 리스트에 포함한 속성만 검색대상 (모든 속성값에 대한 검색 :false)
+        bindings.excludeUnlistedProperties(true);
+        //TODO id, 닉네임
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
--- src/main/java/com/amy/springboard/repository/PostRepository.java
+++ src/main/java/com/amy/springboard/repository/PostRepository.java
@@ -1,9 +1,30 @@
 package com.amy.springboard.repository;
 
 import com.amy.springboard.domain.Post;
+import com.amy.springboard.domain.QPost;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface PostRepository extends JpaRepository<Post, Long> {
+public interface PostRepository extends
+        JpaRepository<Post, Long>,
+        QuerydslPredicateExecutor<Post>
+        , QuerydslBinderCustomizer<QPost> {
+    @Override
+    default void customize(QuerydslBindings bindings, QPost root){
+        // 사용자가 정의한 리스트에 포함한 속성만 검색대상 (모든 속성값에 대한 검색 :false)
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.title,root.content, root.tag, root.createdAt, root.createdBy);
+        //TODO id, 닉네임
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.tag).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
