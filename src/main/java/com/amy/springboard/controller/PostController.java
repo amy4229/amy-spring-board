581c5918d5a08a6671d4cd489410255b8241aff0
amy <amykim4229@gmail.com>
2022-10-26 22:12:13 +0900

    #14 뷰 테스트 작성
    게시글 컨트롤러 작성에 앞서 테스트 정의
    빌드 에러를 막기 위해 각 테스트에 Disabled처리

---
+++ src/main/java/com/amy/springboard/controller/PostController.java
@@ -0,0 +1,16 @@
+package com.amy.springboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/posts")
+public class PostController {
+    /**
+     * TODO
+     * /posts
+     * /posts/{post-id}
+     * /posts/search
+     * /posts/search-tag
+     * */
+}
---
+++ src/test/java/com/amy/springboard/controller/PostControllerTest.java
@@ -0,0 +1,59 @@
+package com.amy.springboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@DisplayName("게시글 뷰 엔드포인트 테스트")
+@WebMvcTest(PostController.class)
+class PostControllerTest {
+    private final MockMvc mvc;
+
+    public PostControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Disabled("구현전")
+    @Test
+    @DisplayName("게시글 리스트 페이지 [GET][정상]")
+    public void getPosts_list_normal() throws Exception {
+        mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("posts"));
+    }
+
+    @Test
+    @Disabled("구현전")
+    @DisplayName("게시글 상세페이지[GET][정상]")
+    public void getPost_detail_normal() throws Exception {
+        mvc.perform(get("/posts/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML))
+                .andExpect(model().attributeExists("post"));
+    }
+    @Test
+    @Disabled("구현전")
+    @DisplayName("게시글 키워드검색페이지[GET][정상]")
+    public void getPost_search_keyword_normal() throws Exception {
+        mvc.perform(get("/posts/search"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+    @Test
+    @Disabled("구현전")
+    @DisplayName("게시글 해시태그 검색 페이지[GET][정상]")
+    public void getPost_search_tag_normal() throws Exception {
+        mvc.perform(get("/posts/search-tag"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.TEXT_HTML));
+    }
+
+}
