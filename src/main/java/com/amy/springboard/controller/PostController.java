fd6f7c8d72dbb3033b4f4eda8484c5f7513ddc7d
amy <amykim4229@gmail.com>
2022-11-29 16:45:22 +0900

    #14 뷰 엔드포인트 테스트를 위한 임시 컨트롤러 메서드 추가

--- src/main/java/com/amy/springboard/controller/PostController.java
+++ src/main/java/com/amy/springboard/controller/PostController.java
@@ -1,16 +1,25 @@
 package com.amy.springboard.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/posts")
 public class PostController {
     /**
      * TODO
-     * /posts
      * /posts/{post-id}
      * /posts/search
      * /posts/search-tag
      * */
+
+    @GetMapping
+    public String posts(ModelMap map){
+        map.addAttribute("posts", List.of());
+        return "posts/index";
+    }
 }
