dc326fe086b576a99f26a441586b95f72fcb7ae3
amy <amykim4229@gmail.com>
2022-12-01 23:41:46 +0900

    #14 로그인 화면 관련 테스트 추가
    스프링 시큐리티에서 제공하는 로그인 화면을 테스트하였다.

---
+++ src/test/java/com/amy/springboard/controller/AuthControllerTest.java
@@ -0,0 +1,32 @@
+package com.amy.springboard.controller;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+@DisplayName("뷰컨트롤러 인증부분 테스트")
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @Test
+    @DisplayName("로그인페이지[정상]")
+    public void noinput_login_page_nomal() throws Exception {
+        mvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
